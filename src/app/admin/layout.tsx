f83b6f3333f5d3b2bf473004fabcb1a952d630c9
Jouhar <jouharvaramangalath786@gmail.com>
2024-11-12 14:37:35 +0530

    feat: Vendors listing page created

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,5 @@
+import RoleSidebar from "@/components/BaseComponents/ui/RoleSidebar/RoleSidebar";
 import React from "react";
-import RoleSidebar from "@/components/baseComponents/ui/RoleSidebar/RoleSidebar";
 
 interface AdminLayoutProps {
   children: React.ReactNode;
@@ -12,7 +12,7 @@ const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
     <div className="flex h-screen overflow-hidden bg-zinc-900">
       <RoleSidebar role={role} />
       <div className="flex-1 bg-black overflow-y-auto">
-        <div className="container mx-auto text-white p-4">{children}</div>
+        <div className="flex w-full h-full">{children}</div>
       </div>
     </div>
   );
