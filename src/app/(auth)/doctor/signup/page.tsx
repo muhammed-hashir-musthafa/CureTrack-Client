d920e2194f424762ee1dee8a367494f0f794607a
erfanpv <erfanpv786@gmail.com>
2024-11-25 16:27:24 +0530

    feat: update admin, vendor, and doctor page components

--- src/app/(auth)/doctor/signup/page.tsx
+++ src/app/(auth)/doctor/signup/page.tsx
@@ -1,36 +1,39 @@
 import React from "react";
 import Image from "next/image";
-import signupBg from "../../../../../public/images/signupBg.png";
 import BaseLogo from "@/components/baseComponents/ui/BaseLogo/BaseLogo";
-import SignupForm from "@/components/doctorComponents/forms/SignupForms/SignupForms";
-  
-const AdminSignupPage = () => {
+import signupBg from "../../../../../public/images/signupBg.png";
+import DoctorSignupForm from "@/components/doctorComponents/forms/SignupForms/SignupForms";
+
+const VendorsSignupPage = () => {
   return (
     <div className="flex flex-col md:flex-row h-screen">
-      <div className="flex flex-1 justify-center items-center bg-customDark text-white p-8 relative">
+      <div className="flex flex-1 justify-center items-center bg-customDark text-white px-6 md:px-8 relative">
         <BaseLogo />
 
-        <div className="w-full max-w-md text-center">
-          <h2 className="text-3xl font-bold mb-2 text-start">Hi there...</h2>
-          <p className="mb-6 text-gray-400 text-start">
-            SignUp & Get Started with Cure Track.
-          </p>
-          <SignupForm />
+        <div className="w-full max-w-md text-center overflow-hidden">
+          <div className="w-full h-full max-h-[80vh] overflow-y-auto scrollbar-custom">
+            <h2 className="text-3xl font-bold mb-2 text-start">Hi there...</h2>
+            <p className="mb-6 text-gray-400 text-start">
+              SignUp & Get Started with Cure Track.
+            </p>
+
+            <DoctorSignupForm />
+          </div>
         </div>
 
-        <p className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-center text-gray-500 text-sm">
+        <p className="absolute bottom-4 md:bottom-8 left-1/2 transform -translate-x-1/2 text-center text-gray-500 text-xs md:text-sm">
           © Cure Track, All Rights Reserved
         </p>
       </div>
 
-      <div className="hidden md:flex flex-1 relative">
+      <div className="hidden md:flex flex-1 relative bg-customDark">
         <div className="absolute top-0 left-0 w-full h-full">
           <Image
             src={signupBg}
             alt="Doctor in background"
             layout="fill"
-            objectFit="contain"
-            objectPosition="right"
+            objectFit="cover"
+            objectPosition="center"
             className="bg-customDark"
           />
         </div>
@@ -39,4 +42,4 @@ const AdminSignupPage = () => {
   );
 };
 
-export default AdminSignupPage;
+export default VendorsSignupPage;
--- src/app/admin/doctors/page.tsx
+++ src/app/admin/doctors/page.tsx
@@ -1,8 +1,12 @@
-import DoctorList from "@/components/AdminComponents/containers/Doctos-List/DoctorList";
+import DoctorsList from "@/components/adminComponents/containers/Doctos-List/DoctorList";
 import React from "react";
 
 const page = () => {
-  return <DoctorList />;
+  return (
+    <div className="flex h-full w-full text-white">
+      <DoctorsList />
+    </div>
+  );
 };
 
 export default page;
