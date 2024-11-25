ce7ba6a5ed81fa0189c373edd457e4f27c54a037
erfanpv <erfanpv786@gmail.com>
2024-11-25 16:28:48 +0530

    chore: update dependencies, interfaces, and global styles

---
+++ src/interfaces/common.ts
@@ -0,0 +1,12 @@
+import { IconType } from "react-icons";
+
+export interface InputFieldProps {
+  label?: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+  icon?: IconType;
+  iconClassName?: string;
+  containerClassName?: string;
+  inputClassName?: string;
+}
---
+++ src/interfaces/doctor.ts
@@ -0,0 +1,7 @@
+export interface DctorSignUpFormValues {
+  fullName: string;
+  IMAId: string;
+  specialization: string;
+  email: string;
+  password: string;
+}
