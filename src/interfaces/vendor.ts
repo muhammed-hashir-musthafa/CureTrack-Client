1a16ad5202e171bdf670b58e60d60a44039cf35e
erfanpv <erfanpv786@gmail.com>
2024-12-06 14:35:30 +0530

    chore: Update configurations, interfaces, and utilities

--- src/interfaces/vendor.ts
+++ src/interfaces/vendor.ts
@@ -1,9 +1,26 @@
 export interface vendorSignupFormValues {
-    name: string;
-    email: string;
-    phoneNumber: string;
-    vendorRole: string;
-    license: string;
-    password: string;
-  }
-  
+  name: string;
+  email: string;
+  phoneNumber: string;
+  vendorRole: string;
+  license: string;
+  password: string;
+}
+
+export interface VendorProfileForm {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  licenseNo: string;
+  address: {
+    buildNo: string;
+    pincode: string;
+    city: string;
+    street: string;
+    state: string;
+    country: string;
+  };
+  licenseDocument: File | null;
+  profileImage: File | null;
+}
